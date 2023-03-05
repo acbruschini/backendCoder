6a445051b47ac9c75b55ca22bde4f3c8fae91c6f
acbruschini <bruschini@gmail.com>
2023-03-05 16:20:17 -0300

    Preentrega 03

---
+++ proyectoFinal/03/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import prodRouter from "./products.js";
+import cartRouter from "./cart.js";
+import userRouter from "./user.js";
+
+export const router = Router();
+
+router.use("/productos", prodRouter);
+router.use("/carrito", cartRouter);
+router.use("/usuario", userRouter);
+
+// export const loggedInRouter = Router();
+
+// loggedInRouter.use("/view", )
